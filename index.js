3b6cc327a2b732dab4614cb06848a1ed5b098aaf
Progga <proggaroy2000@gmail.com>
2023-01-28 00:36:06 +0600

    array index

---
+++ index.js
@@ -0,0 +1,15 @@
+var numbers =[ 89,23,43,55,123,908,1234,51,31,62];
+console.log(numbers);
+console.log(numbers[3]);
+console.log(numbers.indexOf(123));
+
+var num = [33,43,121,98,44,654,66];
+console.log(num);
+console.log("number" + num.length);
+
+// get element by index
+
+var element = num[3];
+console.log(element);
+num[3]= 77;
+console.log(num);
