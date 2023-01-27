3c0f2a023f1d4ae9e4c2bb8ffe9472901cd48ac2
Progga <proggaroy2000@gmail.com>
2023-01-28 01:24:41 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,11 @@
+// console.log(8<=8);
+var num1 =65;
+var num2 = 32;
+console.log(num1>num2);
+console.log(num1<num2);
+console.log(num1==num2);
+console.log(num1>=num2);
+console.log(num1<=num2);
+console.log(num1!=num2);
+
+// multiple condition
