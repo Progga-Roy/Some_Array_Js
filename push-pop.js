0e833463a065caf13594e6d58c68e75b5d34c57c
Progga <proggaroy2000@gmail.com>
2023-01-28 01:09:27 +0600

    a array push-pop

---
+++ push-pop.js
@@ -0,0 +1,13 @@
+var numbers = [90,16,76,49,30,73,67,39,59,62,53];
+var element = numbers.pop(30);
+console.log(element);
+// numbers.push(88);
+// console.log(numbers);
+
+var vegetables = ['alu', 'mula', 'lal-shak','korola','dherosh'];
+vegetables.unshift('lauu')
+vegetables.pop();
+vegetables.push('begun');
+// vegetables.shift();
+console.log(vegetables);
+
