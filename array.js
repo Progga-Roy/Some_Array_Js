b31cf1f941ba535ba11585012347efea8ce62fd3
Progga <proggaroy2000@gmail.com>
2023-01-27 23:48:52 +0600

    array intro

---
+++ array.js
@@ -0,0 +1,6 @@
+var numbers = [12,44,34,89,30];
+var fruits = ['mango', 'orange','lichi'];
+var vowels = ['a','e','i','o','u'];
+console.log(numbers.indexOf(34));
+console.log(vowels);
+console.log(vowels.length);
