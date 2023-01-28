fee7683d492b9bb22de858142539e48f4a95d799
Progga <proggaroy2000@gmail.com>
2023-01-28 20:21:59 +0600

    multi-stage-condition

---
+++ multi-stage-condition.js
@@ -0,0 +1,22 @@
+var myMoney = 120;
+var fuchka =120;
+
+var butterBiscuit = 55;
+
+var cocaCola = 122
+var dadhiChat =135;
+
+if(myMoney > cocaCola){
+    console.log('cocoCola');
+}
+else if(myMoney > butterBiscuit)
+{
+    console.log('butter-biscuit');
+}
+else if(myMoney > dadhiChat)
+{
+    console.log('dahi-chatt');
+}
+else{
+    console.log('konoti e noy')
+}
