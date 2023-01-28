4da627bfac853656ed7ea47dc47dc052334c3da3
Progga <proggaroy2000@gmail.com>
2023-01-28 18:51:31 +0600

    multiple-condition

---
+++ multiple-condition.js
@@ -0,0 +1,29 @@
+var isGraduated = true;
+var salary = 25000;
+var cars = 0;
+
+if (isGraduated = true && salary == 25000 && cars <= 1) {
+    // console.log('Valo chakri nai')
+}
+
+else{
+    // console.log('amr salary kom')
+} 
+
+
+if(isGraduated = false || salary > 30000 || cars == 3){
+    // console.log('Valo chakri nai')
+}
+else{
+    // console.log('amr salary kom')
+}
+
+
+if((isGraduated == true && cars == 0) || salary <= 35000 ) {
+    console.log('Valo chakri nai');
+}
+
+else{
+    console.log('amr salary kom');
+}
+
